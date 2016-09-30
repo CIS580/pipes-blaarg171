51d8208bb8190b721454d2d17492b6100f4f0da2
Lowell Scott <blaarg171@gmail.com>
2016-09-29 20:28:22 -0500

    Added Board

--- src/board.js
+++ src/board.js
@@ -3,10 +3,29 @@
 module.exports = exports = Board;
 
 function Board(width, height, tileSize) {
-  this.width = width;
-  this.height = height;
+  this.width = width / tileSize;
+  this.height = height / tileSize;
   this.tileSize = tileSize;
+  this.spritesheet = new Image();
+  this.spritesheet.src = encodeURI('assets/pipes.png');
 
+  this.tiles = new Array(this.width);
+  for (var i = 0; i < this.width; i++) {
+    this.tiles[i] = new Array(this.height);
+    for (var j = 0; j < this.height; j++) {
+      this.tiles[i][j] = "empty";
+    }
+  }
+}
+
+Board.prototype.render = function (ctx) {
+  for (var x = 0; x < this.width; x++) {
+    for (var y = 0; y < this.height; y++) {
+      switch (this.tiles[x][y]) {
+
+      }
+    }
+  }
 }
 
 Board.prototype.handleClick = function (position) {
--- src/pipe.js
+++ src/pipe.js
@@ -1,2 +1,9 @@
 "use strict";
 
+module.exports = exports = Pipe;
+
+function Pipe(type) {
+  this.type = type;
+  this.spritesheet = new Image();
+  this.spritesheet.src = encodeURI('assets/pipes.png');
+}
